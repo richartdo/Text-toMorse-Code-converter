7a88464c5e87b2f5b89049df30a9bf34175890c3
Brian Richard <146543078+richartdo@users.noreply.github.com>
2024-07-16 07:41:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+# lets have a dictionary of characters with their morse codes.
+morse_codes = {
+    'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.',
+    'g': '--.', 'h': '....', 'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..',
+    'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-', 'r': '.-.',
+    's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-',
+    'y': '-.--', 'z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--',
+    '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...', ':': '---...',
+    ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-',
+    '"': '.-..-.', '$': '...-..-', '@': '.--.-.', ' ': '/'
+}
+
+
+# prompt user to make an input
+user_input = input("Input a string: ").lower()
+
+# create an empty variable
+string_to_morse = ""
+
+for letter in user_input:
+        # for every character in the user input, add its morse code to the empty variable
+        string_to_morse += morse_codes[letter]
+
+# print the morse code
+print(string_to_morse)
